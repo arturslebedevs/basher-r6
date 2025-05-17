bc23185cb7707082a48cc08b4d2e1b4b7f8506b7
Arthur <lebedevs.arturs@gmail.com>
2025-05-17 23:37:27 +0300

    added bot command for version2

--- r6bot/commands.py
+++ r6bot/commands.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 from r6bot.version import BOT_VERSION
 
-@commands.command(name="version")
-async def version_command(ctx):
-    await ctx.send(f"Current fentabot version: `{BOT_VERSION}`")
+def register(bot: commands.Bot):
+    @bot.command(name="version")
+    async def version_command(ctx):
+        await ctx.send(f"Current fentabot version: `{BOT_VERSION}`")
